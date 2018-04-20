ec3566683c16e03727b5be9e1afb34388fa937e0
97Sandy <97sandyrdz@gmail.com>
2018-04-19 21:04:26 -0500

    Initial Commit

---
+++ ThinkingGreenIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThinkingGreenIOS
+//
+//  Created by Sandy on 4/19/18.
+//  Copyright © 2018 Sandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
