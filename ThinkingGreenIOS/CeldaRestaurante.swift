ea42d4b64fcf926ec57917d9774ac576a29264dc
97Sandy <97sandyrdz@gmail.com>
2018-05-02 15:21:34 -0500

    Actualización Collection Views

---
+++ ThinkingGreenIOS/CeldaRestaurante.swift
@@ -0,0 +1,15 @@
+//
+//  CeldaRestaurante.swift
+//  ThinkingGreenIOS
+//
+//  Created by Sandy on 5/2/18.
+//  Copyright © 2018 Sandy. All rights reserved.
+//
+
+import UIKit
+
+class CeldaRestaurante: UICollectionViewCell {
+    
+    @IBOutlet weak var lblRestaurante: UILabel!
+    @IBOutlet weak var tvDesRestaurante: UITextView!
+}
---
+++ ThinkingGreenIOS/restaurantesVC.swift
@@ -0,0 +1,48 @@
+//
+//  restaurantesVC.swift
+//  ThinkingGreenIOS
+//
+//  Created by Sandy on 5/2/18.
+//  Copyright © 2018 Sandy. All rights reserved.
+//
+
+import UIKit
+
+class restaurantesVC: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "celdaRestaurante", for: indexPath) as! CeldaRestaurante
+        
+        return celda
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
